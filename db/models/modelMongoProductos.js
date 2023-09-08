f94dbcdbbfbe326890c50cf1fa3f83ceae5e7660
Jeisson Alexander Desktop <alexamarok260@gmail.com>
2023-09-07 20:01:23 -0500

    Se agrega un cambio para el esquema/ entidad de productos en Mongo DB

--- db/models/modelMongoProductos.js
+++ db/models/modelMongoProductos.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const productos = new Schema({
     timestamp: { type: String, require: true },
     name: { type: String, required: true },
+    category: { type: String, required: true },
     description: { type: String, required: true },
     code: { type: Number, required: true, index: true },
     img: { type: String, required: true },
