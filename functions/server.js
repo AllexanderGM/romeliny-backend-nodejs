91814897d2f6e228825cb7825f05fee149a140af
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-06-26 17:18:23 -0500

    fix: se ajusta para deploy

--- functions/server.js
+++ functions/server.js
@@ -37,9 +37,6 @@ const { port: NODE_PORT } = minimist(process.argv.slice(2), optsMinimist);
 const PORT = normalizePort(NODE_PORT);
 const ENV = process.env.NODE_ENV || "development";
 
-// --> Se configura las rutas path
-const __dirname = path.dirname(__filename);
-
 // * ----- SE INICIA EL SERVIDOR EN EXPRESS ----- * \\
 
 const app = express();
